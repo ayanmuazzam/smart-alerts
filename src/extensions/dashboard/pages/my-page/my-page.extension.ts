eaa0e8fd6aaa053381bde0bc42b5333846b109ba
ayanmuazzam <ayan.muazzam@unitedsol.net>
2026-09-23 11:39:33 +0500

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'c9f52a6c-804b-49d4-a15e-14d6fb006a03',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+  fullPage: false,
+});
